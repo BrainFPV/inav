72ab5cae09585598e7341ad8af8ed85e3331f33f
Martin Luessi <mluessi@gmail.com>
2021-07-10 08:50:06 -0700

    FPGA: fix

--- src/main/target/RADIX2/fpga_drv.h
+++ src/main/target/RADIX2/fpga_drv.h
@@ -36,11 +36,12 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include "common/time.h"
 #include "io/ledstrip.h"
 
 
 int32_t BRAINFPVFPGA_Init(bool load_config);
-uint8_t BRAINFPVFPGA_GetHWRevision();
+uint8_t BRAINFPVFPGA_GetHWRevision(void);
 int32_t BRAINFPVFPGA_SerialRxInvert(bool invert);
 void BRAINFPVFPGA_SetBwLevels(uint8_t black, uint8_t white);
 int32_t BRAINFPVFPGA_SetSyncThreshold(uint8_t threshold);

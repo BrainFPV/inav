48527d0e77b16efad13ecdb372d126a3ebdc4e9b
Martin Luessi <mluessi@gmail.com>
2021-07-10 08:49:30 -0700

    fixes

--- src/main/brainfpv/video.h
+++ src/main/brainfpv/video.h
@@ -33,8 +33,9 @@
 #ifndef VIDEO_H
 #define VIDEO_H
 
-#include "target.h"
-#include <stm32f4xx_qspi.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "platform.h"
 
 // PAL/NTSC specific boundary values
 struct video_type_boundary {
@@ -58,11 +59,12 @@ struct video_type_cfg {
 
 
 void Video_Init(void);
-//void Video_SetLevels(uint8_t, uint8_t, uint8_t, uint8_t);
-//void Video_SetXOffset(int8_t);
-//void Video_SetYOffset(int8_t);
-//void Video_SetXScale(uint8_t pal_x_scale, uint8_t ntsc_x_scale);
-//void Video_Set3DConfig(enum video_3d_mode mode, uint8_t right_eye_x_shift);
+bool VideoIsInitialized(void);
+extern void Video_SetLevels(uint8_t, uint8_t, uint8_t, uint8_t);
+extern void Video_SetXOffset(int8_t);
+extern void Video_SetYOffset(int8_t);
+extern void Video_SetXScale(uint8_t pal_x_scale, uint8_t ntsc_x_scale);
+extern void Video_Set3DConfig(enum video_3d_mode mode, uint8_t right_eye_x_shift);
 
 uint16_t Video_GetLines(void);
 uint16_t Video_GetType(void);
@@ -89,5 +91,8 @@ extern const struct video_type_boundary *video_type_boundary_act;
 #define BUFFER_WIDTH_TMP     (GRAPHICS_WIDTH_REAL / (8 / VIDEO_BITS_PER_PIXEL))
 #define BUFFER_WIDTH (BUFFER_WIDTH_TMP + BUFFER_WIDTH_TMP % 4)
 #define BUFFER_HEIGHT        (GRAPHICS_HEIGHT_REAL)
+
+// Macro to swap buffers given a temporary pointer.
+#define SWAP_BUFFS(tmp, a, b) { tmp = a; a = b; b = tmp; }
 
 #endif /* VIDEO_H */

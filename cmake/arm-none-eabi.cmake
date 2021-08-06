e11b56a52ba228aaabe3daae962305ffe8156f66
Martin Luessi <mluessi@gmail.com>
2021-08-06 14:51:32 -0700

    change debug compile flags

--- cmake/arm-none-eabi.cmake
+++ cmake/arm-none-eabi.cmake
@@ -30,7 +30,7 @@ endif()
 set(CMAKE_BUILD_TYPE ${CMAKE_BUILD_TYPE} CACHE STRING "Build Type" FORCE)
 set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS ${CMAKE_CONFIGURATION_TYPES})
 
-set(arm_none_eabi_debug "-Og -g")
+set(arm_none_eabi_debug "-Og -ggdb -finline-functions-called-once")
 # We set -Os or -O2 depending on the MCU family
 set(arm_none_eabi_release "-DNDEBUG")
 set(arm_none_eabi_relwithdebinfo "-ggdb3 ${arm_none_eabi_release}")

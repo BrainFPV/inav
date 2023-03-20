289b89270ccf9f34da2f058dbe4d6a5e029807ec
Martin Luessi <mluessi@gmail.com>
2023-03-20 14:40:55 -0700

    Fix EXTI2 for F4

--- src/main/drivers/exti.c
+++ src/main/drivers/exti.c
@@ -186,10 +186,8 @@ void EXTI_IRQHandler(uint32_t line_mask)
 
 _EXTI_IRQ_HANDLER(EXTI0_IRQHandler, 0x0001);
 _EXTI_IRQ_HANDLER(EXTI1_IRQHandler, 0x0002);
-#if defined(STM32F7) || defined(STM32H7)
+#if defined(STM32F4) || defined(STM32F7) || defined(STM32H7)
 _EXTI_IRQ_HANDLER(EXTI2_IRQHandler, 0x0004);
-#elif defined(STM32F4)
-_EXTI_IRQ_HANDLER(EXTI2_TS_IRQHandler, 0x0004);
 #else
 # warning "Unknown CPU"
 #endif

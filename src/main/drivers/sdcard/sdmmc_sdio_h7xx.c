589ea8367c45decf0690a974da6c92d7c4e96f5d
Martin Luessi <mluessi@gmail.com>
2024-04-05 08:20:44 -0700

    RADIX 2 HD: Use enable pullups for sdio

--- src/main/drivers/sdcard/sdmmc_sdio_h7xx.c
+++ src/main/drivers/sdcard/sdmmc_sdio_h7xx.c
@@ -146,7 +146,11 @@ void sdioPinConfigure(void)
 #endif
 }
 
+#if defined(SDIO_USE_PULLUP)
+#define IOCFG_SDMMC       IO_CONFIG(GPIO_MODE_AF_PP, GPIO_SPEED_FREQ_VERY_HIGH, GPIO_PULLUP)
+#else
 #define IOCFG_SDMMC       IO_CONFIG(GPIO_MODE_AF_PP, GPIO_SPEED_FREQ_VERY_HIGH, GPIO_NOPULL)
+#endif
 
 void HAL_SD_MspInit(SD_HandleTypeDef* hsd)
 {

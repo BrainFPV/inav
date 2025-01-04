e912933432ce92a717abe85a7068cb138fab52e6
Martin Luessi <mluessi@gmail.com>
2025-01-04 12:36:52 -0800

    BrainFPV: Call taskUpdateBattery before scheduler starts to avoid 40 ms pause in DSHOT output

--- src/main/main_chibios.c
+++ src/main/main_chibios.c
@@ -28,6 +28,7 @@
 #include "drivers/serial.h"
 #include "drivers/serial_softserial.h"
 #include "drivers/persistent.h"
+#include "drivers/time.h"
 
 #include "fc/fc_init.h"
 
@@ -37,6 +38,7 @@
 extern uint32_t __process_stack_end__;
 extern uint32_t __process_stack_base__;
 
+void taskUpdateBattery(timeUs_t currentTimeUs);
 
 volatile bool idleCounterClear = 0;
 volatile uint32_t idleCounter = 0;
@@ -99,6 +101,9 @@ static THD_FUNCTION(InavThread, arg)
         chThdCreateStatic(waOSDThread, sizeof(waOSDThread), NORMALPRIO, OSDThread, NULL);
     }
 #endif /* USE_BRAINFPV_OSD */
+
+    // run battery task once
+    taskUpdateBattery(micros());
 
     // run INAV scheduler forever
     while (true) {
